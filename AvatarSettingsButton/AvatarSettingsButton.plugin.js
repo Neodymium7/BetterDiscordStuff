26e8d6519fd9abfabfe49cf555a9e6e67e5fb4e2
Neodymium <>
2022-04-18 16:08:50 -0700

    AvatarSettingsButton v1.1.1

--- AvatarSettingsButton/AvatarSettingsButton.plugin.js
+++ AvatarSettingsButton/AvatarSettingsButton.plugin.js
@@ -1,37 +1,14 @@
 /**
  * @name AvatarSettingsButton
  * @author Neodymium
- * @version 1.1.0
+ * @version 1.1.1
  * @description Moves the User Settings button to the user avatar, with the status picker and context menu still available on configurable actions
  * @source https://github.com/Neodymium7/BetterDiscordStuff/blob/main/AvatarSettingsButton/AvatarSettingsButton.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Neodymium7/BetterDiscordStuff/main/AvatarSettingsButton/AvatarSettingsButton.plugin.js
  * @invite fRbsqH87Av
  */
-/*@cc_on
-@if (@_jscript)
-    
-    // Offer to self-install for clueless users that try to run this directly.
-    var shell = WScript.CreateObject("WScript.Shell");
-    var fs = new ActiveXObject("Scripting.FileSystemObject");
-    var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
-    var pathSelf = WScript.ScriptFullName;
-    // Put the user at ease by addressing them in the first person
-    shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
-    if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
-        shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
-    } else if (!fs.FolderExists(pathPlugins)) {
-        shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
-    } else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
-        fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
-        // Show the user where to put plugins in the future
-        shell.Exec("explorer " + pathPlugins);
-        shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
-    }
-    WScript.Quit();
 
-@else@*/
-
-module.exports = (() => {
+ module.exports = (() => {
     const config = {
         "info": {
             "name": "AvatarSettingsButton",
@@ -40,13 +17,13 @@ module.exports = (() => {
                     "name": "Neodymium"
                 }
             ],
-            "version": "1.1.0",
+            "version": "1.1.1",
             "description": "Moves the User Settings button to the user avatar, with the status picker and context menu still available on configurable actions",
             "github": "https://github.com/Neodymium7/BetterDiscordStuff/blob/main/AvatarSettingsButton/AvatarSettingsButton.plugin.js",
             "github_raw": "https://raw.githubusercontent.com/Neodymium7/BetterDiscordStuff/main/AvatarSettingsButton/AvatarSettingsButton.plugin.js"
         },
         "changelog": [
-            {"title": "Improved", "type": "improved", "items": ["Rewrote the plugin", "Now works with new Account Profile Popout Experiment"]},
+            {"title": "Fixed", "type": "fixed", "items": ["Fixed account popout not hiding properly with experiment"]},
         ],
         "main": "index.js"
     };
@@ -123,11 +100,11 @@ module.exports = (() => {
                         const openSettings = (e) => {
                             DiscordModules.UserSettingsWindow.setSection("My Account");
                             DiscordModules.UserSettingsWindow.open();
-                            if (document.querySelector("#status-picker")) openStatusPicker(e);
+                            if (document.querySelector("#status-picker") || document.querySelector("#account")) openStatusPicker(e);
                         }
                         const openContextMenu = (e) => {
                             document.querySelector(settingsSelector).dispatchEvent(new MouseEvent("contextmenu", {bubbles: true, clientX: e.clientX, clientY: (screen.height - 12)}));
-                            if (document.querySelector("#status-picker")) openStatusPicker(e);
+                            if (document.querySelector("#status-picker") || document.querySelector("#account")) openStatusPicker(e);
                         }
                         const actions = [null, openSettings, openContextMenu, openStatusPicker];
                         const tooltips = ["", "User Settings", "Settings Shortcuts", "Set Status"];
