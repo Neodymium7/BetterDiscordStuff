d653f088f5d24a55bf7eedb171418c02e09bbfce
Neodymium <68879269+Neodymium7@users.noreply.github.com>
2022-06-14 17:18:39 -0700

    CopyCodeblocks v1.0.3

--- CopyCodeblocks/CopyCodeblocks.plugin.js
+++ CopyCodeblocks/CopyCodeblocks.plugin.js
@@ -1,7 +1,7 @@
 /**
  * @name CopyCodeblocks
  * @author Neodymium
- * @version 1.0.1
+ * @version 1.0.3
  * @description Adds a copy button to codeblocks.
  * @source https://github.com/Neodymium7/BetterDiscordStuff/blob/main/CopyCodeblocks/CopyCodeblocks.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Neodymium7/BetterDiscordStuff/main/CopyCodeblocks/CopyCodeblocks.plugin.js
@@ -10,66 +10,57 @@
 
 const { TooltipContainer } = BdApi.findModuleByProps("TooltipContainer");
 const Copy = BdApi.findModuleByDisplayName("Copy");
+const copy = BdApi.findModuleByProps("requireModule").copy;
 const { React } = BdApi;
 
-class Codeblock extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {text: "Copy Code", color: "primary", forceOpen: false};
-    }
+function Codeblock(props) {
+	const [copied, setCopied] = React.useState(false);
+	const [forceOpen, setForceOpen] = React.useState(false);
 
-    resetTooltip() {
-        this.setState({forceOpen: false});
-        setTimeout(() => {
-            this.setState({text: "Copy Code", color: "primary"});
-        }, 50);
-    }
+	const resetCopied = () => {
+		setForceOpen(false);
+		setTimeout(() => setCopied(false), 50);
+	};
 
-    render() {
-        return React.createElement("div", {"class": "codeblockWrapper", onMouseLeave: () => { this.resetTooltip(); }}, 
-            React.createElement(TooltipContainer, {position: "top", text: this.state.text, color: this.state.color, forceOpen: this.state.forceOpen, className: "copyCodeblockButtonWrapper"}, 
-                React.createElement("div", 
-                    {
-                        "class": "copyCodeblockButton", 
-                        onClick: () => {
-                            require("electron").clipboard.writeText(this.props.content);
-                            this.setState({text: "Copied!", color: "green", forceOpen: true});
-                            setTimeout(() => { this.resetTooltip(); }, 1000);
-                        }
-                    }, 
-                React.createElement(Copy, {width: 18, height: 18}))
-            ),
-            this.props.innerHTML && React.createElement("div", {className: "codeblockContent", dangerouslySetInnerHTML: this.props.innerHTML}),
-            !(this.props.innerHTML) && React.createElement("div", {className: "codeblockContent"}, this.props.content)
-        );
-    }
+	const copyCode = () => {
+		copy(props.content);
+		setCopied(true);
+		setForceOpen(true);
+		setTimeout(resetCopied, 1000);
+	};
+
+	return React.createElement(
+		"div",
+		{ className: "codeblockWrapper", onMouseLeave: resetCopied },
+		React.createElement(
+			TooltipContainer,
+			{
+				position: "top",
+				text: copied ? "Copied!" : "Copy Code",
+				color: copied ? "green" : "primary",
+				forceOpen: forceOpen,
+				className: "copyCodeblockButtonWrapper"
+			},
+			React.createElement("div", { className: "copyCodeblockButton", onClick: copyCode }, React.createElement(Copy, { width: "18", height: "18" }))
+		),
+		props.innerHTML
+			? React.createElement("div", { className: "codeblockContent", dangerouslySetInnerHTML: props.innerHTML })
+			: React.createElement("div", { className: "codeblockContent" }, props.content)
+	);
 }
 
 module.exports = class CopyCodeblocks {
-    start() {
-        BdApi.injectCSS("CopyCodeblocks", `
+	start() {
+		BdApi.injectCSS(
+			"CopyCodeblocks",
+			`
             .codeblockWrapper {
                 position: relative;
                 margin: -7px;
             }
             .codeblockContent {
-                padding: 7px;
-                max-width: 100%;
-                overflow-x: scroll;
-                white-space: pre !important;
-            }
-            .codeblockContent::-webkit-scrollbar {
-                width: 4px;
-                height: 4px;
-            }
-            .codeblockContent::-webkit-scrollbar-thumb {
-                background-color: rgba(24,25,28,.6);
-                border-radius: 2px;
-                cursor: move;
-            }
-            .codeblockContent::-webkit-scrollbar-track {
-                background-color: transparent;
-            }
+                padding: 7px
+			}
             .copyCodeblockButtonWrapper {
                 background-color: var(--background-tertiary);
                 position: absolute;
@@ -100,22 +91,23 @@ module.exports = class CopyCodeblocks {
             .codeblockWrapper:not(:hover) .copyCodeblockButtonWrapper {
                 display: none;
             }
-        `);
-        const Parser = BdApi.findModuleByProps("parseTopic");
-        BdApi.Patcher.after("CopyCodeblocks", Parser.defaultRules.codeBlock, "react", (_, [{ content }], ret) => {
-            const render = ret.props.render;
-            ret.props.render = renderProps => {
-                const codeblock = render(renderProps);
-                const innerHTML = codeblock.props.children.props.dangerouslySetInnerHTML;
-                delete codeblock.props.children.props.dangerouslySetInnerHTML;
-                codeblock.props.children.props.children = React.createElement(Codeblock, {content: content, innerHTML: innerHTML});
-                return codeblock;
-            }
-        });
-    }
+        `
+		);
+		const Parser = BdApi.findModuleByProps("parseTopic");
+		BdApi.Patcher.after("CopyCodeblocks", Parser.defaultRules.codeBlock, "react", (_, [{ content }], ret) => {
+			const render = ret.props.render;
+			ret.props.render = renderProps => {
+				const codeblock = render(renderProps);
+				const innerHTML = codeblock.props.children.props.dangerouslySetInnerHTML;
+				delete codeblock.props.children.props.dangerouslySetInnerHTML;
+				codeblock.props.children.props.children = React.createElement(Codeblock, { content: content, innerHTML: innerHTML });
+				return codeblock;
+			};
+		});
+	}
 
-    stop() {
-        BdApi.Patcher.unpatchAll("CopyCodeblocks");
-        BdApi.clearCSS("CopyCodeblocks");
-    }
-}
+	stop() {
+		BdApi.Patcher.unpatchAll("CopyCodeblocks");
+		BdApi.clearCSS("CopyCodeblocks");
+	}
+};
