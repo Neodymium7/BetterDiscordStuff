5d7dd1cd98fe9e994649dd17515268f73c0dc2cd
Neodymium <68879269+Neodymium7@users.noreply.github.com>
2022-10-03 17:24:07 -0700

    TypingUsersPopouts v1.2.0

--- TypingUsersPopouts/TypingUsersPopouts.plugin.js
+++ TypingUsersPopouts/TypingUsersPopouts.plugin.js
@@ -1,123 +1,125 @@
 /**
  * @name TypingUsersPopouts
  * @author Neodymium
- * @version 1.1.2
+ * @version 1.2.0
  * @description Opens the user's popout when clicking on a name in the typing area.
  * @source https://github.com/Neodymium7/BetterDiscordStuff/blob/main/TypingUsersPopouts/TypingUsersPopouts.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Neodymium7/BetterDiscordStuff/main/TypingUsersPopouts/TypingUsersPopouts.plugin.js
  * @invite fRbsqH87Av
  */
 
-module.exports = (() => {
-    const config = {
-        "info": {
-            "name": "TypingUsersPopouts",
-            "authors": [
-                {
-                    "name": "Neodymium"
-                }
-            ],
-            "version": "1.1.2",
-            "description": "Opens the user's popout when clicking on a name in the typing area.",
-            "github": "https://github.com/Neodymium7/BetterDiscordStuff/blob/main/TypingUsersPopouts/TypingUsersPopouts.plugin.js",
-            "github_raw": "https://raw.githubusercontent.com/Neodymium7/BetterDiscordStuff/main/TypingUsersPopouts/TypingUsersPopouts.plugin.js"
-        },
-        "changelog": [
-            {"title": "Fixed", "type": "fixed", "items": ["Fixed plugin after Discord's changes to the typing area", "Should also be more resilient to future changes"]},
-        ]
-    };
+/*@cc_on
+@if (@_jscript)
+    
+    // Offer to self-install for clueless users that try to run this directly.
+	var shell = WScript.CreateObject("WScript.Shell");
+	var fs = new ActiveXObject("Scripting.FileSystemObject");
+	var pathPlugins = shell.ExpandEnvironmentStrings("%APPDATA%\\BetterDiscord\\plugins");
+	var pathSelf = WScript.ScriptFullName;
+	// Put the user at ease by addressing them in the first person
+	shell.Popup("It looks like you've mistakenly tried to run me directly. \n(Don't do that!)", 0, "I'm a plugin for BetterDiscord", 0x30);
+	if (fs.GetParentFolderName(pathSelf) === fs.GetAbsolutePathName(pathPlugins)) {
+		shell.Popup("I'm in the correct folder already.", 0, "I'm already installed", 0x40);
+	} else if (!fs.FolderExists(pathPlugins)) {
+		shell.Popup("I can't find the BetterDiscord plugins folder.\nAre you sure it's even installed?", 0, "Can't install myself", 0x10);
+	} else if (shell.Popup("Should I copy myself to BetterDiscord's plugins folder for you?", 0, "Do you need some help?", 0x34) === 6) {
+		fs.CopyFile(pathSelf, fs.BuildPath(pathPlugins, fs.GetFileName(pathSelf)), true);
+		// Show the user where to put plugins in the future
+		shell.Exec("explorer " + pathPlugins);
+		shell.Popup("I'm installed!", 0, "Successfully installed", 0x40);
+	}
+	WScript.Quit();
+@else@*/
 
-    return !global.ZeresPluginLibrary ? class {
-        constructor() { this._config = config; }
-        getName() { return config.info.name; }
-        getAuthor() { return config.info.authors.map(a => a.name).join(", "); }
-        getDescription() { return config.info.description; }
-        getVersion() { return config.info.version; }
-        load() {
-            BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
-                confirmText: "Download Now",
-                cancelText: "Cancel",
-                onConfirm: () => {
-                    require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
-                        if (error) return require("electron").shell.openExternal("https://betterdiscord.net/ghdl?url=https://raw.githubusercontent.com/rauenzi/BDPluginLibrary/master/release/0PluginLibrary.plugin.js");
-                        await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
-                    });
-                }
+const config = {
+	info: {
+		name: "TypingUsersPopouts",
+		authors: [{ name: "Neodymium" }],
+		version: "1.2.0",
+		description: "Opens the user's popout when clicking on a name in the typing area.",
+		github: "https://github.com/Neodymium7/BetterDiscordStuff/blob/main/TypingUsersPopouts/TypingUsersPopouts.plugin.js",
+		github_raw: "https://raw.githubusercontent.com/Neodymium7/BetterDiscordStuff/main/TypingUsersPopouts/TypingUsersPopouts.plugin.js"
+	},
+	changelog: [{
+		title: "Fixed",
+		type: "fixed",
+		items: ["Rewritten to fix compatibility with Discord's latest update."]
+	}]
+};
+
+if (!global.ZeresPluginLibrary) {
+    BdApi.showConfirmationModal("Library Missing", `The library plugin needed for ${config.info.name} is missing. Please click Download Now to install it.`, {
+        confirmText: "Download Now",
+        cancelText: "Cancel",
+        onConfirm: () => {
+            require("request").get("https://rauenzi.github.io/BDPluginLibrary/release/0PluginLibrary.plugin.js", async (error, response, body) => {
+                if (error) return require("electron").shell.openExternal("https://betterdiscord.app/Download?id=9");
+                await new Promise(r => require("fs").writeFile(require("path").join(BdApi.Plugins.folder, "0PluginLibrary.plugin.js"), body, r));
             });
         }
-        start() {}
-        stop() {}
-    } : (([Plugin, Api]) => {
-        const plugin = (Plugin, Library) => {
+    });
+}
 
-            const { DiscordModules, DiscordSelectors, Patcher, Popouts, ReactComponents, Utilities } = Library;
+function buildPlugin([Plugin, Library]) {
+	const { DiscordModules, DiscordSelectors, Patcher, ReactComponents, Utilities } = Library;
 
-            const fetchProfile = BdApi.findModuleByProps("fetchProfile").fetchProfile;
-            const getUserProfile = BdApi.findModuleByProps("getUserProfile").getUserProfile;
+	const { React, Webpack, injectCSS, clearCSS } = BdApi
+	const { Filters: { byStrings }, getModule } = Webpack;
 
-            const LoadingPopout = BdApi.findModuleByDisplayName("LoadingPopout");
+	const UserPopout = getModule(byStrings(".canViewThemes?"));
+	const Popout = getModule(byStrings(".animationPosition"));
 
-            const nameSelector = `${DiscordSelectors.Typing.typing} strong`;
-            const avatarSelector = `${DiscordSelectors.UserPopout.userPopout} > :nth-child(2)`;
+	const nameSelector = `${DiscordSelectors.Typing.typing} strong`;
 
-            let currentPopoutId;
+	const { UserStore, RelationshipStore } = DiscordModules;
 
-            function remove(e) { // Quick fix for popout not closing when it should
-                const inPopout = document.querySelector("#ZeresPluginLibraryPopouts").contains(e.target);
-                const isName = Array.from(document.querySelectorAll(nameSelector)).some(node => node.isEqualNode(e.target));
-            
-                if (!inPopout && !isName) Popouts.closePopout(currentPopoutId);
-                else if (document.querySelector(avatarSelector).contains(e.target) && e.button === 0) {
-                    document.querySelector(avatarSelector).click();
-                    Popouts.closePopout(currentPopoutId);
-                }
-                else if (!isName) document.addEventListener("mouseup", remove, {once: true});
-            }
-        
-            return class TypingUsersPopouts extends Plugin {
-                constructor() {
-                    super();
-                }
-            
-                onStart() {
-                    this.patch();
-                    BdApi.injectCSS("TypingUsersPopouts", `${nameSelector}{cursor: pointer;} ${nameSelector}:hover{text-decoration: underline;}`);
-                }
-            
-                onStop() {
-                    Patcher.unpatchAll();
-                    BdApi.clearCSS("TypingUsersPopouts");
-                }
+	return class TypingUsersPopouts extends Plugin {
+		onStart() {
+			injectCSS(
+				"TypingUsersPopouts",
+				`${nameSelector} { cursor: pointer; } ${nameSelector}:hover { text-decoration: underline; }`
+			);
+			this.patch();
+		}
 
-                async patch() {
-                    const TypingUsers = await ReactComponents.getComponentByName("TypingUsers", DiscordSelectors.Typing.typing);
-                    Patcher.after(TypingUsers.component.prototype, 'render', (component, [props], ret) => {
-                        const typingUsers = Object.keys(component.props.typingUsers).filter(id => id !== DiscordModules.UserStore.getCurrentUser().id && !DiscordModules.RelationshipStore.isBlocked(id));
-                        const names = Utilities.findInTree(ret, e => Array.isArray(e) && e.length > 0 && e[0].type === "strong")?.filter(child => child.type === "strong");
-                        if (!names) return;
-                        for (let i = 0; i < typingUsers.length; i++) {
-                            names[i].props.className = `typing-user-${typingUsers[i]}`;
-                            names[i].props.onClick = async e => {
-                                if (document.querySelector(`#ZeresPluginLibraryPopouts ${DiscordSelectors.UserPopout.userPopout}[data-user-id="${typingUsers[i]}"]`)) return;
-                                const name = document.querySelector(`.typing-user-${typingUsers[i]}`);
-                                e.stopPropagation();
-                                if (currentPopoutId) await Popouts.closePopout(currentPopoutId);
-                                if (!getUserProfile(typingUsers[i])) {
-                                    const loadingId = Popouts.openPopout(name, {position: "top", align: "left", spacing: 8, render: () => BdApi.React.createElement(LoadingPopout)});
-                                    await fetchProfile(typingUsers[i]);
-                                    Popouts.closePopout(loadingId);
-                                }
-                                currentPopoutId = Popouts.openPopout(name, {position: "top", align: "left", nudgeAlignIntoViewport: true, spacing: 8, animation: Popouts.AnimationTypes.TRANSLATE, render: () => BdApi.React.createElement(DiscordModules.UserPopout, {userId: typingUsers[i], guildId: DiscordModules.SelectedGuildStore.getGuildId(), channelId: DiscordModules.SelectedChannelStore.getChannelId()})});
-                                document.addEventListener("mouseup", remove, {once: true}); // Quick fix for popout not closing when it should
-                            }
-                        }
-                    });
-                }
-            
-            };
+		async patch() {
+			const TypingUsers = await ReactComponents.getComponent(
+				"TypingUsers",
+				DiscordSelectors.Typing.typing,
+				(c) => c.prototype?.getCooldownTextStyle
+			);
+			Patcher.after(TypingUsers.component.prototype, "render", (thisObject, _, ret) => {
+				const typingUsersIds = Object.keys(thisObject.props.typingUsers).filter(
+					(id) => id !== UserStore.getCurrentUser().id && !RelationshipStore.isBlocked(id)
+				);
+				const text = Utilities.findInReactTree(ret, (e) => e.children?.length && e.children[0]?.type === "strong");
+				if (!text) return ret;
 
-        };
-        return plugin(Plugin, Api);
-    })(global.ZeresPluginLibrary.buildPlugin(config));
-})();
+				let i = 0;
+				text.children = text.children.map((e) => {
+					if (e.type !== "strong") return e;
+					const userId = typingUsersIds[i++];
+
+					return React.createElement(
+						Popout, 
+						{ 
+							align: "left", 
+							position: "top", 
+							key: userId, 
+							renderPopout: () => React.createElement(UserPopout, { userId, guildId: thisObject.props.channel.guild_id }) 
+						}, 
+						(props) => React.createElement("strong", { ...props, ...e.props })
+					);
+				});
+			});
+		}
+
+		onStop() {
+			clearCSS("TypingUsersPopouts");
+			Patcher.unpatchAll();
+		}
+	}
+}
+
+module.exports = global.ZeresPluginLibrary ? buildPlugin(global.ZeresPluginLibrary.buildPlugin(config)) : class { start() {}; stop() {} };
 /*@end@*/
