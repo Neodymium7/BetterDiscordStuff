f8436736933835a8abac015ce6dce6cbf1d40382
Neodymium <68879269+Neodymium7@users.noreply.github.com>
2022-10-11 16:13:08 -0700

    PinnedMessageIcons v1.0.3

--- PinnedMessageIcons/PinnedMessageIcons.plugin.js
+++ PinnedMessageIcons/PinnedMessageIcons.plugin.js
@@ -1,14 +1,16 @@
 /**
  * @name PinnedMessageIcons
  * @author Neodymium
- * @version 1.0.2
+ * @version 1.0.3
  * @description Displays an icon on and adds a class to pinned messages. (Heavily inspired by PinIcon by Qwerasd, go check out their plugin!)
  * @source https://github.com/Neodymium7/BetterDiscordStuff/blob/main/PinnedMessageIcons/PinnedMessageIcons.plugin.js
  * @updateUrl https://raw.githubusercontent.com/Neodymium7/BetterDiscordStuff/main/PinnedMessageIcons/PinnedMessageIcons.plugin.js
  * @invite fRbsqH87Av
  */
 
-const Pin = BdApi.findModuleByDisplayName("Pin");
+const { getModule, Filters: { byStrings } } = BdApi.Webpack;
+
+const Pin = getModule(byStrings("M22 12L12.101 2.10101L10.686 3.51401L12.101 4.92901L7.15096"));
 
 module.exports = class PinnedMessageIcons {
     start() {
@@ -19,8 +21,8 @@ module.exports = class PinnedMessageIcons {
             right: 16px;
             color: var(--interactive-normal);
         }`);
-        const Message = BdApi.findModule(m => m.default && m.default.toString && m.default.toString().includes("childrenRepliedMessage"));
-        BdApi.Patcher.after("PinnedMessageIcons", Message, "default", (_, [props], ret) => {
+        const Message = getModule((m) => Object.values(m).some(p => p?.toString?.().includes("childrenRepliedMessage")));
+        BdApi.Patcher.after("PinnedMessageIcons", Message, "Z", (_, [props], ret) => {
             const isPinned = props.childrenMessageContent.props.message.pinned;
             if (isPinned && Array.isArray(ret.props.children.props.children) && !props["data-list-item-id"].includes("pin")) {
                 ret.props.children.props.className += " pinned-message";
