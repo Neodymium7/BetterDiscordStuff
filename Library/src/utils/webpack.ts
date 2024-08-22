6f0401f56420bc3d458d2721add7c8d118371365
Neodymium <68879269+Neodymium7@users.noreply.github.com>
2024-08-21 22:38:33 -0700

    Fix again

--- Library/src/utils/webpack.ts
+++ Library/src/utils/webpack.ts
@@ -183,7 +183,7 @@ export function bySourceStrings(...strings: string[]) {
  */
 export function getIcon(name: string, searchString: string): (props: IconProps) => JSX.Element {
 	return expectModule<any>({
-		filter: (m) => Webpack.Filters.byStrings(searchString) && typeof m == "function",
+		filter: (m) => Webpack.Filters.byStrings(searchString)(m) && typeof m == "function",
 		name,
 		fallback: (_props: IconProps) => null,
 		searchExports: true,
