dfc5b0f332ddce2a956cef77679a11da6cd6740b
Neodymium <68879269+Neodymium7@users.noreply.github.com>
2025-04-07 19:46:33 -0700

    Fix popout component

--- common/discord/components.tsx
+++ common/discord/components.tsx
@@ -2,7 +2,7 @@ import { EmptyWrapperComponent, ErrorPopout } from "@lib/utils/react";
 import { expectModule } from "@lib/utils/webpack";
 
 export const Popout = /* @__PURE__ */ expectModule({
-	filter: (m) => m.defaultProps && m.Animation?.TRANSLATE,
+	filter: (m) => m.defaultProps && m.prototype.shouldShowPopout,
 	name: "Popout",
 	fallback: EmptyWrapperComponent,
 	searchExports: true,
